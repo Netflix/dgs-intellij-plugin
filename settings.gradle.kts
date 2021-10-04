28846161bb02bd0c01e45f1da2d1497b799f9ebd
GitHub Action <action@github.com>
2021-10-04 17:27:30 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "dgs-intellij-plugin"
--- src/main/kotlin/com/github/paulbakker/dgsintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paulbakker/dgsintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paulbakker.dgsintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paulbakker.dgsintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
